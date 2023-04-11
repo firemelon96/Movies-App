44b319006c4389f3f5efad35febe08602d884016
estong jamion <estong.jamion@gmail.com>
2023-04-11 15:02:10 +0300

    Added firebase realtime database for send/request data

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMov = useCallback(async function () {
+    setError(null);
+    setIsLoading(true);
+    try {
+      const res = await fetch(
+        "https://react-http-7e647-default-rtdb.firebaseio.com/movies.json"
+      );
+      if (!res.ok) throw new Error("Something went wrong");
+      const data = await res.json();
+      const transformeData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformeData);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMov();
+  }, [fetchMov]);
+
+  const addMovieHandler = (movie) => {
+    console.log(movie);
+  };
+
+  let content = <p>No Movies Found</p>;
+
+  if (movies.length > 0) content = <MoviesList movies={movies} />;
+
+  if (error) content = <p>{error}</p>;
+
+  if (isLoading) content = <p>Loading...</p>;
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <button onClick={fetchMov}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
